aa4a3d50de57fbd7b80250ab5c6b204cdadec381
freakZ112 <102925064+freakZ112@users.noreply.github.com>
2023-02-21 16:53:51 +0100

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,77 @@
+const fum_url = "https://scripts.drtraxx.de", /* live */
+//const fum_url = "http://localhost/www/scrpts.drtraxx", /* dev */
+    fum_datestring = new Date().getTime();
+
+var aVehicleTypesNew = aVehicleTypesNew || [];
+
+(async function () {
+
+    await $.getScript(`${ fum_url }/fuhrparkmanager/variables_functions.js?${ fum_datestring }`);
+
+    const securityCheck = await checkUserId(user_id);
+
+    if (securityCheck.locked === true) {
+        alert(`Hallo ${ securityCheck.name },\nDu darfst mein Script nicht nutzen!`);
+        window.location.reload;
+        return false;
+    }
+
+    // temporÃ¤re Nachricht an Nutzer
+    if (!sessionStorage.msg_traxx) {
+        $("body")
+            .prepend(`<div class="modal modal_fum" tabindex="-1" role="dialog" style="display:block !important">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Wichtige Mitteilung zum Fuhrpark-Manager</h5>
+                                    <button type="button" class="close" onclick="$('.modal_fum').remove()" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Lieber ${ user_name }</p>
+                                    <br>
+                                    <p>Wie du sicherlich schon mitbekommen hast, ist mir aktuell der Support meines Scripts nicht mehr mÃ¶glich, da mein Account im Spiel, so wie im Forum gebannt wurde.</p>
+                                    <p>Leider Ã¤uÃŸert sich der Support des Spiels nicht zu den UmstÃ¤nden des Banns, auf welchem Wege ich es auch versuche.</p>
+                                    <p>Aus diesem Grund stelle ich aktuell den Support aller meiner Scripts bis auf weiteres ein.</p>
+                                    <p>Sollte mich der Support bis Ende diesen Monats weiter ignorieren, werde ich auch die Funktion aller meiner Scripts einstellen.</p>
+                                    <p>Ich bitte um Dein VerstÃ¤ndnis und hoffe, dass sich bald alles aufklÃ¤rt, so dass ich wie gewohnt weiter supporten und entwickeln kann.</p>
+                                    <p>Bis dahin stehe ich  Dir gern unter support@drtraxx.de zur VerfÃ¼gung.</p>
+                                    <br>
+                                    <p>Liebe GrÃ¼ÃŸe</p>
+                                    <p>DrTraxx</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" onclick="$('.modal_fum').remove()">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`);
+        sessionStorage.msg_traxx = "message shown";
+    }
+
+    if (aVehicleTypesNew.length === 0) {
+        if (!localStorage.aVehicleTypesNew || JSON.parse(localStorage.aVehicleTypesNew).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) {
+            await $.getJSON("https://api.lss-cockpit.de/de_DE/vehicletypes.json").done(data => localStorage.setItem('aVehicleTypesNew', JSON.stringify({ lastUpdate: new Date().getTime(), value: data })));
+        }
+        aVehicleTypesNew = JSON.parse(localStorage.aVehicleTypesNew).value;
+    }
+
+    if (aVehicles.length === 0) {
+        getVehiclesApi();
+    }
+
+    if (aBuildings.length === 0) {
+        aBuildings = await getApi("buildings");
+    }
+
+    if (aCredits.length === 0) {
+        aCredits = await getApi("credits");
+    }
+
+    $('head').append(`<link rel="stylesheet" href="${ fum_url }/fuhrparkmanager/main.css?${ new Date().getTime() }" type="text/css" />`);
+
+    await $.getScript(`${ fum_url }/fuhrparkmanager/modal.js?${ fum_datestring }`);
+    await $.getScript(`${ fum_url }/fuhrparkmanager/initial_functions.js?${ fum_datestring }`);
+    await $.getScript(`${ fum_url }/fuhrparkmanager/onclick.js?${ fum_datestring }`);
+})();
