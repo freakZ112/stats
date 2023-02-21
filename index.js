dda7531758ac77e45e653d64a5eab6affbf838d7
freakZ112 <102925064+freakZ112@users.noreply.github.com>
2023-02-21 16:30:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ var aVehicleTypesNew = aVehicleTypesNew || [];
 
     if (aVehicleTypesNew.length === 0) {
         if (!localStorage.aVehicleTypesNew || JSON.parse(localStorage.aVehicleTypesNew).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) {
-            await $.getJSON("https://raw.githubusercontent.com/freakZ112/vehicletypes/main/vehicletypes.json").done(data => localStorage.setItem('aVehicleTypesNew', JSON.stringify({ lastUpdate: new Date().getTime(), value: data }))); // hier kann man auch meine json einfügen
+            await $.getJSON("https://api.lss-cockpit.de/de_DE/vehicletypes.json").done(data => localStorage.setItem('aVehicleTypesNew', JSON.stringify({ lastUpdate: new Date().getTime(), value: data }))); // hier kann man auch meine json einfügen
 			        }
         aVehicleTypesNew = JSON.parse(localStorage.aVehicleTypesNew).value;
     }
